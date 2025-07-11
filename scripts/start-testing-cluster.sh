88922f5acebe59fdad3a2487e867f110f806f891
Alex Hytha <ahytha@relaynetwork.com>
2025-07-11 12:23:56 -0400

    got UI set up in testing suite

--- scripts/start-testing-cluster.sh
+++ scripts/start-testing-cluster.sh
@@ -6,6 +6,16 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 DOCKER_COMPOSE_FILE="$SCRIPT_DIR/../testing/docker-compose.yml"
 
+(
+  cd "$SCRIPT_DIR/../channel-ui" || exit 1
+  npm run build
+  if [ $? -ne 0 ]; then
+    echo "Channel UI build failed. Exiting."
+    exit 1
+  fi
+  echo "Channel UI build completed successfully."
+)
+
 echo "Starting testing Docker cluster..."
 
 docker-compose -f "$DOCKER_COMPOSE_FILE" up -d --build
